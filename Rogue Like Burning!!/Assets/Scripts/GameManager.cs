773948fc0e5d1db869eb467e4abcc9e0d3ec98db
MaxBurning <max-burning@outlook.jp>
2016-05-03 00:20:36 +0900

    GameManager.csを新しく作ったよよよ

---
+++ Rogue Like Burning!!/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ゲーム内のあれこれを行うクラス
+/// </summary>
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+    // スタートボタンに相当するものが押されたらメニューを開く
+    void OpenMenu()
+    {
+
+    }
+}
